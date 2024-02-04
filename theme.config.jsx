21dbca76c25b6ac9e92dc969335c63bb03b6788e
Atri <atriusedm@gmail.com>
2024-02-04 15:47:35 -0500

    chore: Init project

---
+++ theme.config.jsx
@@ -0,0 +1,11 @@
+export default {
+    docsRepositoryBase: 'https://github.com/Biosense-Collective/docs/tree/main',
+    project: {
+        link: 'https://github.com/Biosense-Collective'
+    },
+    logo: <b>Biosense Collective</b>,
+    primaryHue: {
+        dark: 300,
+        light: 250,
+    }
+}
