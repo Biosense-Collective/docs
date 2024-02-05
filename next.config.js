0a352f8a49286b8d1342a25b71cfe8b904888bda
Atri <atriusedm@gmail.com>
2024-02-05 03:43:20 -0500

    chore: Nextjs config changes

--- next.config.js
+++ next.config.js
@@ -3,10 +3,18 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.jsx',
 })
 
+const isProduction = process.env.NODE_ENV === "production";
+const assetPrefix = isProduction ? "/docs" : "";
+
 module.exports = {
     ...withNextra(),
     images: {
         unoptimized: true,
     },
+    reactStrictMode: true,
+    swcMinify: true,
+    trailingSlash: true,
+    assetPrefix,
+    basePath: assetPrefix,
     output: "export",
 }
